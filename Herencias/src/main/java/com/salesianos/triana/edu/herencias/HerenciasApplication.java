02cfebe016264639edab60d1bd0abd93de492f44
Crenstonw <b2016rubensa@gmail.com>
2023-11-03 13:27:44 +0100

    proyecto añadido

---
+++ Herencias/src/main/java/com/salesianos/triana/edu/herencias/HerenciasApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.edu.herencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerenciasApplication.class, args);
+	}
+
+}
